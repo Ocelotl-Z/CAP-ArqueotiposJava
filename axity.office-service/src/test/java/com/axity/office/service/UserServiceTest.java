adae7c9b56f16390a724d1fd169f59d64db43aa8
Ocelotl <ocelotlmagick@gmail.com>
2023-03-24 15:27:50 -0600

    Test & Ignore Metals config

--- axity.office-service/src/test/java/com/axity/office/service/UserServiceTest.java
+++ axity.office-service/src/test/java/com/axity/office/service/UserServiceTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.axity.office.commons.dto.RoleDto;
 import com.axity.office.commons.dto.UserDto;
 import com.axity.office.commons.enums.ErrorCode;
 import com.axity.office.commons.exception.BusinessException;
@@ -31,114 +34,122 @@ import com.axity.office.commons.request.PaginatedRequestDto;
  */
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Transactional
-class UserServiceTest
-{
-  private static final Logger LOG = LoggerFactory.getLogger( UserServiceTest.class );
+class UserServiceTest {
+	private static final Logger LOG = LoggerFactory.getLogger(UserServiceTest.class);
 
-  @Autowired
-  private UserService userService;
+	@Autowired
+	private UserService userService;
 
-  /**
-   * Method to validate the paginated search
-   */
-  @Test
-  void testFindUsers()
-  {
-    var request = new PaginatedRequestDto();
-    request.setLimit( 5 );
-    request.setOffset( 0 );
-    var users = this.userService.findUsers( request );
+	private RoleDto creatRole(int id) {
+		var role = new RoleDto();
+		role.setId(id);
+		return role;
+	}
 
-    LOG.info( "Response: {}", users );
+	/**
+	 * Method to validate the paginated search
+	 */
+	@Test
+	void testFindUsers() {
+		var request = new PaginatedRequestDto();
+		request.setLimit(5);
+		request.setOffset(0);
+		var users = this.userService.findUsers(request);
 
-    assertNotNull( users );
-    assertNotNull( users.getData() );
-    assertFalse( users.getData().isEmpty() );
-  }
+		LOG.info("Response: {}", users);
 
-  /**
-   * Method to validate the search by id
-   * 
-   * @param userId
-   */
-  @ParameterizedTest
-  @ValueSource(ints = { 1 })
-  void testFind( Integer userId )
-  {
-    var user = this.userService.find( userId );
-    assertNotNull( user );
-    LOG.info( "Response: {}", user );
-  }
+		assertNotNull(users);
+		assertNotNull(users.getData());
+		assertFalse(users.getData().isEmpty());
+	}
 
-  /**
-   * Method to validate the search by id inexistent
-   */
-  @Test
-  void testFind_NotExists()
-  {
-    var user = this.userService.find( 999999 );
-    assertNull( user );
-  }
+	/**
+	 * Method to validate the search by id
+	 * 
+	 * @param userId
+	 */
+	@ParameterizedTest
+	@ValueSource(ints = { 1 })
+	void testFind(Integer userId) {
+		var user = this.userService.find(userId);
+		assertNotNull(user);
+		LOG.info("Response: {}", user);
+	}
 
-  /**
-   * Test method for
-   * {@link com.axity.office.service.impl.UserServiceImpl#create(com.axity.office.commons.dto.UserDto)}.
-   */
-  @Test
-  @Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
-  void testCreate()
-  {
-    var dto = new UserDto();
-    // Crear de acuerdo a la entidad
+	/**
+	 * Method to validate the search by id inexistent
+	 */
+	@Test
+	void testFind_NotExists() {
+		var user = this.userService.find(999999);
+		assertNull(user);
+	}
 
-    var response = this.userService.create( dto );
-    assertNotNull( response );
-    assertEquals( 0, response.getHeader().getCode() );
-    assertNotNull( response.getBody() );
+	/**
+	 * Test method for
+	 * {@link com.axity.office.service.impl.UserServiceImpl#create(com.axity.office.commons.dto.UserDto)}.
+	 */
+	@Test
+	// @Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
+	void testCreate() {
+		var dto = new UserDto();
+		var roles = new ArrayList<RoleDto>();
 
-    this.userService.delete( dto.getId() );
-  }
+		roles.add(creatRole(1));
+		roles.add(creatRole(2));
 
-  /**
-   * Method to validate update
-   */
-  @Test
-  @Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
-  void testUpdate()
-  {
-    var user = this.userService.find( 1 ).getBody();
-    // TODO: actualizar de acuerdo a la entidad
+		dto.setName("Prueba");
+		dto.setUsername("Pruebita");
+		dto.setEmail("some@email.com");
+		dto.setLastName("Test");
+		dto.setRoles(roles);
 
-    var response = this.userService.update( user );
+		var response = this.userService.create(dto);
+		assertNotNull(response);
+		assertEquals(0, response.getHeader().getCode());
+		assertNotNull(response.getBody());
 
-    assertNotNull( response );
-    assertEquals( 0, response.getHeader().getCode() );
-    assertTrue( response.getBody() );
-    user = this.userService.find( 1 ).getBody();
+		this.userService.delete(dto.getId());
+	}
 
-    // Verificar que se actualice el valor
-  }
+	/**
+	 * Method to validate update
+	 */
+	@Test
+	@Disabled("TODO: Actualizar la prueba de acuerdo a la entidad")
+	void testUpdate() {
+		var user = this.userService.find(1).getBody();
+		// TODO: actualizar de acuerdo a la entidad
 
-  /**
-   * Method to validate an inexistent registry
-   */
-  @Test
-  void testUpdate_NotFound()
-  {
-    var user = new UserDto();
-    user.setId(999999);
-    var ex = assertThrows( BusinessException.class, () -> this.userService.update( user ) );
+		var response = this.userService.update(user);
 
-    assertEquals( ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode() );
-  }
+		assertNotNull(response);
+		assertEquals(0, response.getHeader().getCode());
+		assertTrue(response.getBody());
+		user = this.userService.find(1).getBody();
 
-  /**
-   * Test method for {@link com.axity.office.service.impl.UserServiceImpl#delete(java.lang.String)}.
-   */
-  @Test
-  void testDeleteNotFound()
-  {
-    var ex = assertThrows( BusinessException.class, () -> this.userService.delete( 999999 ) );
-    assertEquals( ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode() );
-  }
+		// Verificar que se actualice el valor
+	}
+
+	/**
+	 * Method to validate an inexistent registry
+	 */
+	@Test
+	void testUpdate_NotFound() {
+		var user = new UserDto();
+		user.setId(999999);
+		var ex = assertThrows(BusinessException.class, () -> this.userService.update(user));
+
+		assertEquals(ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.axity.office.service.impl.UserServiceImpl#delete(java.lang.String)}.
+	 */
+	@Test
+	void testDeleteNotFound() {
+		var ex = assertThrows(BusinessException.class, () -> this.userService.delete(999999));
+		assertEquals(ErrorCode.OFFICE_NOT_FOUND.getCode(), ex.getCode());
+	}
 }
